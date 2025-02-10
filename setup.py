5f471e9b0be3734d5427ee55d03b9b4f1f8deeb9
parthiban1417 <parthiban1315@gmail.com>
2025-02-10 12:14:52 +0530

    project setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='MLPipeline',
+    version='0.0.1',
+    author='Parthi',
+    author_email='parthiban1315@gmail.com',
+    packages=find_packages(),
+    instal_requires=[]
+)
